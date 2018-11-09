290c61bae8cbf462d612d03ac8a8a879f14361c9
Oleg Bondar <olegbondar1998@gmail.com>
2018-11-09 20:32:08 +0200

    Merge conflict in Main controller

--- app/controller/Main.php
+++ app/controller/Main.php
@@ -2,11 +2,7 @@
 
 namespace app\controller;
 
-<<<<<<< HEAD
-class Main implements Controller {
-=======
 class Main extends SecuredController {
->>>>>>> upoli
 
 	static function index($req, $res, $service, $app) {
 		return $app->twig->render("index.html", ["user" => $app->user]);
