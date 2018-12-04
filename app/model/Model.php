b7722da18e4c069459a05a18af6463e121c68cd4
Oleg Bondar <olegbondar1998@gmail.com>
2018-12-04 17:21:11 +0200

    Model is now json serializable

--- app/model/Model.php
+++ app/model/Model.php
@@ -2,7 +2,7 @@
 
 namespace app\model;
 
-class Model {
+class Model implements \JsonSerializable {
 
 	public function __construct(array $attributes = []) {
 		foreach ($attributes as $key => $value) $this->$key = $value;
@@ -46,5 +46,17 @@ class Model {
                     break;
             }
 	    }
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
     }
 }
