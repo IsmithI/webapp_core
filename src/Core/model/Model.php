91cb8e96d06c5a3e01c9b83ebb492a9412e75ccd
Oleg Bondar <olegbondar1998@gmail.com>
2018-12-16 16:08:47 +0200

    Added isJson method to Model

--- src/Core/model/Model.php
+++ src/Core/model/Model.php
@@ -86,4 +86,9 @@ class Model implements \JsonSerializable {
     {
         return $this->toArray();
     }
+
+    public static function isJson($string) {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
 }
